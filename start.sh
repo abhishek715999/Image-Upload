65c9a91e9736eefe06f158119bab9cba8ce2f430
abhishek715999 <110544396+abhishek715999@users.noreply.github.com>
2022-10-02 16:30:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/Image-Upload-Bot.git /Image-Upload-Bot
-cd /Image-Upload-Bot
+git clone https://github.com/abhishek715999/Image-Upload.git /Image-Upload
+cd /Image-Upload
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
